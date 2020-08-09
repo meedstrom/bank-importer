b3e2c653fdf132e216e221a137faf2249f9e467d
meedstrom <meedstrom@teknik.io>
2020-08-09 21:39:39 +0200

    Remove unnecessary libraries

--- common.R
+++ common.R
@@ -1,19 +1,12 @@
 suppressPackageStartupMessages({
-  library(codetools) # for some Stan auto-checks
-  library(rethinking, lib.loc="~/R")
   library(rlang)
   library(vctrs)
   library(withr)
   library(slider)
   library(testthat)
   library(magrittr) # mask testthat::not
-  library(bayesplot)
-  library(brms)
-  library(patchwork)
   library(lubridate)
   library(tidyverse)
-  library(tidybayes)
-  library(tidymodels)
 })
 
 set.seed(646)
